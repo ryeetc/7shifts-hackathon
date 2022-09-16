81a6196e00ac712e3f4de4f080d28f4d5e316046
Christef Desir <christefdesir@gmail.com>
2022-09-16 07:53:41 -0400

    made Milestone page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Profile from './pages/Profile/Profile';
 import './App.scss';
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
-import TaskCard from './components/TaskCard/TaskCard';
+import Profile from './pages/Profile/Profile';
+import Milestone from './pages/Milestone/Milestone';
+
 function App() {
   return (
   
     <BrowserRouter>
       <Routes>
         <Route path ="/"  element = {<Profile />}/>
-        {/* <Route path ="/milestone"  element = {<Milestone />}/>
-        <Route path ="/employees"  element = {<Employees />}/>
-        <Route path ="/*" element ={} /> */}
+         <Route path ="/milestone"  element = {<Milestone />}/> 
+        {/* <Route path ="/employees"  element = {<Employees />}/> */}
+        {/* <Route path ="/*" element ={} />  */}
       </Routes>
-      <TaskCard />
       <Navbar />
    </BrowserRouter>
   );
--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,29 +1,17 @@
 import './TaskCard.scss';
-import communication from '../../assets/images/communication.svg'
-import {useState} from 'react';
-
-
-
-
+import communication from '../../assets/images/communication.svg';
+import blurredOne from '../../assets/images/blurred-1.svg';
+import blurredTwo from '../../assets/images/blurred-2.svg';
 
 
 function TaskCard() {
-  const [accomplished, setAccomplished] = useState(false)
-  function handleAccomplished () {
-    setAccomplished(true)
-    console.log("hi")
-  }
+ 
   return (
     <div className='card__section'>
-      <div className='card'>
-        <img className='' 
-         src={communication}
-         alt="communication skills"
-         onClick={handleAccomplished}  />
-      </div>
-      {accomplished && <div className='card'><img className='card__img card__image--selected' src={communication} alt="communication skills" /></div>}
-      <div className='card'><img className='card__img' src={communication} alt="communication skills" /></div>
-      <div className='card'><img className='card__img' src={communication} alt="communication skills" /></div>
+      <div className='card'><img className='card__img'src={communication} alt="communication skills" /></div>
+      <div className='card'><img className='card__img' src={blurredOne} alt="blurred" /></div>
+      <div className='card'><img className='card__img' src={blurredTwo} alt="blurred2" /></div>
+      {/* <div className='card'><img className='card__img' src={communication} alt="blurred communication" /></div> */}
     </div>
   )
 }

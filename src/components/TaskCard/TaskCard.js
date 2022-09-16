26e71486462d8983e0dbd9fb1ad9f7fb32a53f1a
Robert Loo <rloolee89@gmail.com>
2022-09-16 11:43:40 -0400

    Add styling to task card

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,14 +1,14 @@
 import './TaskCard.scss';
-import communication from '../../assets/images/communication.svg';
-import blurredOne from '../../assets/images/blurred-1.svg';
-import blurredTwo from '../../assets/images/blurred-2.svg';
+import blurredOne from '../../assets/images/image-2.png';
+import blurredTwo from '../../assets/images/image-3.png';
+import successImage from '../../assets/images/Component-4.svg';
 
 
 function TaskCard() {
  
   return (
     <div className='card__section'>
-      <div className='card'><img className='card__img'src={communication} alt="communication skills" /></div>
+      <div className='card'><img className='card__img card-success'src={successImage} alt="communication skills" /></div>
       <div className='card'><img className='card__img' src={blurredOne} alt="blurred" /></div>
       <div className='card'><img className='card__img' src={blurredTwo} alt="blurred2" /></div>
       {/* <div className='card'><img className='card__img' src={communication} alt="blurred communication" /></div> */}
--- src/pages/Milestone/Milestone.js
+++ src/pages/Milestone/Milestone.js
@@ -1,13 +1,20 @@
-import GoalHeader from '../../components/GoalHeader/GoalHeader'
 import Navbar from '../../components/Navbar/Navbar'
 import TaskCard from '../../components/TaskCard/TaskCard'
 import './Milestone.scss'
+import arrowIcon from "../../assets/icons/arrow-left.png";
+import {Link} from 'react-router-dom';
+
 
 function Milestone() {
   return (
     <main className="milestone">
-      <GoalHeader />
+    <div className='goal-header'>
+      <Link to='/profile'><img className ="goal-header__arrow"src={arrowIcon} alt="arrow" /></Link>
+      <h3 className='goal-header__header'>My Goal</h3>
+    </div>
+      <div className="milestone-container">
       <h2 className="milestone__subheader">Improve Communication Skills</h2>
+      </div>
       <TaskCard />
       <Navbar />
     </main>
